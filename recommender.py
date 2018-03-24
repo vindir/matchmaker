5e25c04f8306936006bdb9c4bdf0b5497d2e5892
Shae Erisson <shae@scannedinavian.com>
2018-03-24 19:26:22 -0400

    add code demonstrating how to match up shared titles

--- recommender.py
+++ recommender.py
@@ -20,7 +20,7 @@ class Recommender():
 
         sorted_matches = sorted(matches.items(), key=operator.itemgetter(1))
         return sorted_matches[0][0]
-        
+
 
 movies = [ 'Moana', 'Frying Nemo', 'Pocahontas', 'ToyStory', 'LionKing', 'Frozen' ]
 reviewers = {
@@ -38,3 +38,29 @@ matchmaker = Recommender(reviewers, my_picks)
 favorite_reviewer = matchmaker.make_me_a_match()
 print favorite_reviewer
 
+# assume input is two lists of tuples: [(title,score)]
+def movies_in_common(movie_list1, movie_list2):
+    movie_dict1 = dict(movie_list1)
+    movie_dict2 = dict(movie_list2)
+    titles1 = set(movie_dict1.keys())
+    titles2 = set(movie_dict2.keys())
+    common_titles = titles1.intersection(titles2)
+    return common_titles # returns a set of titles
+
+# assume input is two lists of tuples
+def order_matched_titles_and_scores(movie_list1, movie_list2):
+    common_titles = movies_in_common(movie_list1, movie_list2)
+    common_movies1 = common(movie_list1, common_titles)
+    common_movies2 = common(movie_list2, common_titles)
+    return (sorted(common_movies1), sorted(common_movies2))
+
+def common(movie_list, titles):
+    return [m for m in movie_list if m[0] in titles]
+
+
+m1 = [('Moana',1), ('Frying Nemo',0), ('Pocahontas',0)]
+m2 = [('Moana',0), ('Frying Nemo',0), ('ToyStory',1)]
+
+def check():
+    assert order_matched_titles_and_scores(m1,m2) == ([('Frying Nemo',0),('Moana',1)], [('Frying Nemo',0), ('Moana',0)])
+    return "Yup, it works or you would have seen a failed assertion"
